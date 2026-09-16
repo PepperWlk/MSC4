54a61bd84c76fe965a1ca28801bda95f0fee18f6
PepperWlk <lguillemart@gaming.tech>
2026-09-16 17:04:20 +0200

    ADD Player Strike, Mob Wandering, Mob death, Interact Interface

---
+++ KenshiLike/Source/KenshiLike/KenshiLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KenshiLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KenshiLike, "KenshiLike" );
